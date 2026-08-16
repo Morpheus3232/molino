df11ef46334e99bed06f85b51ff83c466f832b44
Franco Viegas <francoviegaslloveras@gmail.com>
2026-08-16 03:53:16 -0300

    fix(design): complete compass icon color replacement in OG images

--- app/compatibility/[entity]/opengraph-image.tsx
+++ app/compatibility/[entity]/opengraph-image.tsx
@@ -37,9 +37,9 @@ export default async function Image({ params }: { params: Promise<{ entity: stri
           <path d="M34,96 L66,96 L56,46 L44,46 Z" fill="#241F17" />
           <path d="M44,46 Q50,34 56,46 Z" fill="#241F17" />
           <g transform="translate(50 38) rotate(45)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#241F17" /></g>
-          <g transform="translate(50 38) rotate(135)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#F3F1EA" /></g>
-          <g transform="translate(50 38) rotate(225)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#F3F1EA" /></g>
-          <g transform="translate(50 38) rotate(315)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#F3F1EA" /></g>
+          <g transform="translate(50 38) rotate(135)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#241F17" /></g>
+          <g transform="translate(50 38) rotate(225)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#241F17" /></g>
+          <g transform="translate(50 38) rotate(315)"><path d="M0,0 L-3,-5 L-4,-11 L-2,-20 L0,-26 L2,-20 L4,-11 L3,-5 Z" fill="#241F17" /></g>
           <circle cx="50" cy="38" r="3.2" fill="#241F17" />
         </svg>
         <div style={{ display: "flex", alignItems: "baseline", gap: 8 }}>
